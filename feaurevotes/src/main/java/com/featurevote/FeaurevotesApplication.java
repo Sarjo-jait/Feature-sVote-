28041a755c09828e92a0d5e703c8eb476e709845
sarjo-jait <sarjo.jaiteh56@gmail>
2020-09-20 19:27:37 +0000

    initial check-in of the freshvote

---
+++ feaurevotes/src/main/java/com/featurevote/FeaurevotesApplication.java
@@ -0,0 +1,13 @@
+package com.featurevote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeaurevotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeaurevotesApplication.class, args);
+	}
+
+}
